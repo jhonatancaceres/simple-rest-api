ee4139553b4ca34ef2730609e8d2eca61b95dae8
jhonatancaceres <jhonatan.caceres@globant.com>
2025-08-17 12:59:29 -0600

    included static funcitonality numberOfFails

--- src/controllers/processController.ts
+++ src/controllers/processController.ts
@@ -1,6 +1,20 @@
 import { Request, Response } from 'express';
 
-const responses: { [path: string]: any } = {};
+const responses: { [path: string]: { numberOfFails: number, counter: number } } = {};
+
+const failResponse = {
+  statusCode: 0,
+  statusText: 'failed',
+  errorCode: 100,
+  errorMessage: 'Unexpected error'
+};
+
+const successResponse = {
+  statusCode: 1,
+  statusText: 'success',
+  errorCode: 0,
+  errorMessage: null
+};
 
 export const processController = {
 
@@ -8,10 +22,11 @@ export const processController = {
     res.json(responses);
   },
   setDefaults: async (req: Request, res: Response) => {
-    const flow = req.body.flow;
-    const process = req.body.process;
-    const path = `${flow}/${process}`;
-    responses[path] = req.body.response;
+
+    Object.entries<any>(req.body.processes).forEach(([path, value]) => {
+      responses[path] = { numberOfFails: value.numberOfFails || 0, counter: 0 };
+    });
+
     res.json(responses);
   },
   createElement: async (req: Request, res: Response) => {
@@ -20,17 +35,18 @@ export const processController = {
     const process = req.body.process;
     const path = `${flow}/${process}`;
 
-    if (responses && responses[path]) {
-      res.json(responses[path]);
+    if (!responses[path]) {
+      responses[path] = { numberOfFails: 0, counter: 0 };
+    }
+
+    const response = responses[path];
+
+    if (response.counter < response.numberOfFails) {
+      response.counter = response.counter + 1;
+      res.json(failResponse);
     } else {
-      const response = {
-        path,
-        statusCode: 1,
-        statusText: 'success',
-        errorCode: 0,
-        errorMessage: ''
-      }
-      res.json(response);
+      response.counter = 0;
+      res.json(successResponse);
     }
   },
 };
